bebdfa564502ebc98b0d9802677cceedc20cff5f
KoluaGylchyk <43492309+KoluaGylchyk@users.noreply.github.com>
2023-08-01 19:45:06 +0300

    delete a href

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -104,22 +104,20 @@ const Navbar = ({ user }: Props) => {
                 </div>
               </div>
               <div className="hidden md:flex items-center">
-                <a href="#_" className="relative inline-block text-lg group">
+                <Link
+                  to="/login"
+                  className="relative inline-block text-lg group"
+                >
                   <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 group-hover:text-white">
                     <span className="absolute inset-0 w-full h-full px-5 py-3  bg-gray-50"></span>
                     <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-                    <span className="relative">
-                      {" "}
-                      <a href="/login" className=" ">
-                        login
-                      </a>
-                    </span>
+                    <span className="relative">login</span>
                   </span>
                   <span
                     className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900  group-hover:mb-0 group-hover:mr-0"
                     data-rounded=""
                   ></span>
-                </a>
+                </Link>
               </div>
               <button className="inline-block md:hidden w-14 h-14">
                 <img src={IconButton} alt="mobile-icon" />
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Carousel } from "../../components";
+import { Link } from "react-router-dom";
 import sliderOne from "../../assets/sliderOne.jpg";
 import sliderTwo from "../../assets/sliderTwo.jpg";
 import sliderThe from "../../assets/sliderThe.jpg";
@@ -31,22 +32,17 @@ const Home = () => {
           </p>
 
           <button className="pl-32 w-auto pt-20  ">
-            <a href="#_" className="relative inline-block text-lg group">
+            <Link to="/login" className="relative inline-block text-lg group">
               <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 group-hover:text-white">
                 <span className="absolute inset-0 w-full h-full px-5 py-3  bg-gray-50"></span>
                 <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-                <span className="relative">
-                  {" "}
-                  <a href="/login" className="">
-                    Sing in
-                  </a>
-                </span>
+                <span className="relative">Sing in</span>
               </span>
               <span
                 className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900  group-hover:mb-0 group-hover:mr-0"
                 data-rounded=""
               ></span>
-            </a>
+            </Link>
           </button>
           {/* <div className="flex ">
             <img
