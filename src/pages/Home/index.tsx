31e50f761f2c58949e37d98589bd762099615d6d
bohdanhulchuknl <bohdanhulchuknl@gmail.com>
2023-08-04 19:05:56 +0300

    select master 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,13 @@
 import { HomeCarousel } from "../../components";
-import { About, Servises, CarouselWork } from "../../components/Home";
+// import { About, Servises, CarouselWork } from "../../components/Home"; //! CarouselWork мішало
+import { About, Servises } from "../../components/Home";
 
-import workOne from "../../assets/halloween-nails.jpg";
-import workTwo from "../../assets/nails2.jpg";
-import workThe from "../../assets/nails3.jpg";
-import workFry from "../../assets/nails4.jpg";
+
+//! мішало
+// import workOne from "../../assets/halloween-nails.jpg";
+// import workTwo from "../../assets/nails2.jpg";
+// import workThe from "../../assets/nails3.jpg";
+// import workFry from "../../assets/nails4.jpg";
 
 import sliderOne from "../../assets/sliderOne.svg";
 import sliderTwo from "../../assets/sliderTwo.jpg";
@@ -12,7 +15,8 @@ import sliderThe from "../../assets/sliderThe.jpg";
 // import sliderFive from "../../assets/sliderFive.jpeg";
 // import sliderSix from "../../assets/sliderSix.jpeg";
 
-const sliderWork = [workOne, workTwo, workThe, workFry];
+// const sliderWork = [workOne, workTwo, workThe, workFry]; //! sliderWork мішало
+
 const slider = [sliderOne, sliderTwo, sliderThe];
 
 const Home = () => {
